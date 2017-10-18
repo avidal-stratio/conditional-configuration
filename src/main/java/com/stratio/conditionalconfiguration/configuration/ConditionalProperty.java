5b5ca0ec7cc79069aab5b17261224bf15a80d76f
Antonio Vidal <avidal@stratio.com>
2017-10-18 14:51:54 +0200

    Conditional Properties

--- src/main/java/com/stratio/conditionalconfiguration/configuration/ConditionalProperty.java
+++ src/main/java/com/stratio/conditionalconfiguration/configuration/ConditionalProperty.java
@@ -1,4 +1,24 @@
 package com.stratio.conditionalconfiguration.configuration;
 
-public class ConditionalProperty {
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.vault.authentication.ClientAuthentication;
+import org.springframework.vault.client.VaultEndpoint;
+import org.springframework.vault.config.AbstractVaultConfiguration;
+
+@Configuration
+@Slf4j
+@ConditionalOnProperty(prefix = "conditional.properties", name = "exist")
+public class ConditionalProperty extends AbstractVaultConfiguration {
+
+    @Override public VaultEndpoint vaultEndpoint() {
+        log.info("Conditional Property -> Exist");
+        return null;
+    }
+
+    @Override public ClientAuthentication clientAuthentication() {
+        log.info("Conditional Property -> Exist");
+        return null;
+    }
 }
